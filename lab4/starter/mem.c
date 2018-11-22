6be1d9c18764dc31e89ec531ac94bbf0914a6b02
Daivik Goel <daivikgoel98@gmail.com>
2018-11-22 18:38:11 -0500

    Working on implementing linked list

--- lab4/starter/mem.c
+++ lab4/starter/mem.c
@@ -1,8 +1,8 @@
 /**
  * @file memory.c
  * @brief: ECE254 Lab: memory allocation algorithm comparison template file
- * @author: 
- * @date: 2015-11-20
+ * @author: Ryan Michael Schmied and Daivik Goel
+ * @date: 2018-11-22
  */
 
 /* includes */
@@ -11,25 +11,36 @@
 #include "mem.h"
 
 /* defines */
+struct ll_node {
+	int size; // size of memory
+	int state; // if the linked list is free or not
+	struct ll_node* previous; // points to previous node
+	struct ll_node* next; // points to next node
 
+}
 /* global variables */
-
-
+ll_node* best;
+ll_node* worst
 /* Functions */
+
 
 /* memory initializer */
 int best_fit_memory_init(size_t size)
 {
+	best = malloc(size); // allocate the memory space
+	
+	struct node* first = best;
+
+
 
 	// To be completed by students
 	// You call malloc once here to obtain the memory to be managed.
-	return 0;
-
+	
 }
 
 int worst_fit_memory_init(size_t size)
 {
-
+	worst = malloc(size);
 	// To be completed by students
 	// You call malloc once here to obtain the memory to be managed.
 	return 0;
