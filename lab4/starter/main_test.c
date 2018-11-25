f35f7a4c0f6c809b4c244c9f7aff3597e3947ed0
Ryan Michael Schmied <rmschmie@ecelinux7.uwaterloo.ca>
2018-11-24 22:20:31 -0500

    finished implementation need to make last test case work

--- lab4/starter/main_test.c
+++ lab4/starter/main_test.c
@@ -2,98 +2,619 @@
   * ECE254 Linux Dynamic Memory Management Lab
   * @file: main_test.c
   * @brief: The main file to write tests of memory allocation algorithms
-  */ 
+  */
 
 /* includes */
 /* system provided header files. You may add more */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 /* non-system provided header files. 
    Do not include more user-defined header files here
  */
 #include "mem.h"
+// TODO: void printCurrentAllocs(){}
+
+// tests completely filling the mem space and deallocates two blocks and then allocates one into the larger free block
+void test1_worst_fit()
+{
+	printf("Test 1 Worst Fit \n");
+	worst_fit_memory_init(1000);
+
+	void *a = worst_fit_alloc(200);
+	worst_fit_alloc(220);
+	void *c = worst_fit_alloc(220);
+	worst_fit_alloc(200);
+
+	printf("Full\n");
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	worst_fit_dealloc(a);
+	worst_fit_dealloc(c);
+
+	printf("Freed blocks 1 and 3\n");
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	worst_fit_alloc(100);
+	printf("Allocated 100, should go into block 3\n");
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	return;
+}
+
+//tests deallocation and merging free blocks
+//fills mem space then deallocates blocks from left to right and merges
+void test2_worst_fit()
+{
+	printf("Test 2 Worst Fit \n");
+	worst_fit_memory_init(1000);
+
+	void *a = worst_fit_alloc(200);
+	void *b = worst_fit_alloc(220);
+	void *c = worst_fit_alloc(220);
+	void *d = worst_fit_alloc(200);
+
+	printf("Full\n");
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	worst_fit_dealloc(a);
+	printf("Freed block 1\n");
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	worst_fit_dealloc(b);
+	printf("Freed block 2\n");
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	worst_fit_dealloc(c);
+	printf("Freed block 3\n");
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	worst_fit_dealloc(d);
+	printf("Freed block 4, all free\n");
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	return;
+}
+
+//tests deallocation with free blocks on either side
+void test3_worst_fit()
+{
+	printf("Test 3 Worst Fit \n");
+	worst_fit_memory_init(1000);
+
+	void *a = worst_fit_alloc(200);
+	void *b = worst_fit_alloc(220);
+	void *c = worst_fit_alloc(220);
+	worst_fit_alloc(200);
+
+	printf("Full\n");
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	worst_fit_dealloc(a);
+	printf("Freed block 1\n");
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	worst_fit_dealloc(c);
+	printf("Freed block 3\n");
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	worst_fit_dealloc(b);
+	printf("Freed block 2\n");
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	return;
+}
+
+//tests to make sure we allocate in incrments of 4 and that we can handle fragmentation
+//Note: there is 28 bytes not being controlled since we need a min of 40 for a control node
+void test4_worst_fit()
+{
+	printf("Test 4 Worst Fit \n");
+	worst_fit_memory_init(1000);
+
+	worst_fit_alloc(201);
+	worst_fit_alloc(202);
+	worst_fit_alloc(203);
+	worst_fit_alloc(203);
+
+	printCurrentAllocs(0);
+	printf("---\n");
+}
+
+//trys to ask for too much memory
+void test5_worst_fit()
+{
+	printf("Test 5 Worst Fit \n");
+	worst_fit_memory_init(1000);
+
+	worst_fit_alloc(200);
+	worst_fit_alloc(500);
+	worst_fit_alloc(100);
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	printf("Asking for the last block which is too much mem\n");
+	worst_fit_alloc(300);
+
+	printCurrentAllocs(0);
+	printf("---\n");
+}
+
+//allocating/deallocating memory and looking at how many fragments
+void test6_worst_fit()
+{
+	printf("Test 6 Worst Fit \n");
+	worst_fit_memory_init(1000);
+
+	worst_fit_alloc(100);
+	void *b = worst_fit_alloc(150);
+	worst_fit_alloc(200);
+	worst_fit_alloc(100);
+	worst_fit_alloc(100);
+	worst_fit_alloc(40);
+
+	worst_fit_dealloc(b);
+	worst_fit_alloc(100);
+
+	printf("Number of free blocks with under 100 bytes: %d \n", worst_fit_count_extfrag(100));
+	printCurrentAllocs(0);
+	printf("---\n");
+}
+
+void test7_worst_fit()
+{ // tests for invalid starting size (less than a control node)
+	printf("Test 7 Worst Fit \n");
+	int result = worst_fit_memory_init(30);
+	printf("0 if memory init worked, -1 if it did not: %d \n", result);
+	printf("---\n");
+}
+
+void test8_worst_fit()
+{ //allocates some memory and uses it
+	printf("Test 8 Worst Fit \n");
+	int result = worst_fit_memory_init(1000);
+	if (result == -1)
+	{
+		return;
+	}
+	void *a = worst_fit_alloc(10);
+	*(int *)a = 25;
+	printf("Should say 25: %d \n", *(int *)a);
+	printf("---\n");
+
+	void *b = worst_fit_alloc(10);
+	*(int *)b = 20;
+
+	*(int *)a = *(int *)a + *(int *)b;
+	printf("Should say 45: %d \n", *(int *)a);
+	printf("---\n");
+
+	worst_fit_dealloc(b);
+	printf("Should say 45: %d \n", *(int *)a);
+	printf("---\n");
+}
+
+//Tests wherther or not we can handle being passed an incorrrect start address for deallocation
+void test9_worst_fit()
+{
+	printf("Test 9 Worst Fit \n");
+	worst_fit_memory_init(1000);
+
+	worst_fit_alloc(200);
+	void *b = worst_fit_alloc(220);
+	worst_fit_alloc(220);
+	worst_fit_alloc(200);
+
+	printf("Allocations: \n");
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	printf("Trying to deallocate a block that is not the starting address we gave\n");
+	worst_fit_dealloc(b + 12);
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	printf("Deallocating the same block of memory by giving the correct start address\n");
+	worst_fit_dealloc(b);
+	printCurrentAllocs(0);
+	printf("---\n");
+}
 
 void test1_best_fit()
 {
-		printf("Test 1 Best Fit \n");
-		best_fit_memory_init(1000);
+	printf("Test 1 Best Fit \n");
+	best_fit_memory_init(1000);
 
-		void *a = best_fit_alloc(200);
-		void *b = best_fit_alloc(200);
-		void *c = best_fit_alloc(200);
-		void *d = best_fit_alloc(200);
+	void *a = best_fit_alloc(200);
+	best_fit_alloc(220);
+	void *c = best_fit_alloc(220);
+	best_fit_alloc(200);
 
-		printf("Full\n");
-		printCurrentAllocs(1);
-		printf("---\n");
+	printf("Full\n");
+	printCurrentAllocs(1);
+	printf("---\n");
 
-		best_fit_dealloc(a);
-		best_fit_dealloc(c);
+	best_fit_dealloc(a);
+	best_fit_dealloc(c);
 
-		printf("Freed blocks 1 and 3\n");
-		printCurrentAllocs(1);
-		printf("---\n");
+	printf("Freed blocks 1 and 3\n");
+	printCurrentAllocs(1);
+	printf("---\n");
 
-		//best_fit_alloc(100);
-		printf("Allocated 100, should go into block 1\n");
-		printCurrentAllocs(1);
-		printf("---\n");
+	best_fit_alloc(100);
+	printf("Allocated 100, should go into block 1\n");
+	printCurrentAllocs(1);
+	printf("---\n");
 
+	return;
+}
+
+void test2_best_fit()
+{
+	printf("Test 2 Best Fit \n");
+	best_fit_memory_init(1000);
+
+	void *a = best_fit_alloc(200);
+	void *b = best_fit_alloc(220);
+	void *c = best_fit_alloc(220);
+	void *d = best_fit_alloc(200);
+
+	printf("Full\n");
+	printCurrentAllocs(1);
+	printf("---\n");
+
+	best_fit_dealloc(a);
+	printf("Freed block 1\n");
+	printCurrentAllocs(1);
+	printf("---\n");
+
+	best_fit_dealloc(b);
+	printf("Freed block 2\n");
+	printCurrentAllocs(1);
+	printf("---\n");
+
+	best_fit_dealloc(c);
+	printf("Freed block 3\n");
+	printCurrentAllocs(1);
+	printf("---\n");
+
+	best_fit_dealloc(d);
+	printf("Freed block 4, all free\n");
+	printCurrentAllocs(1);
+	printf("---\n");
+
+	return;
+}
+
+void test3_best_fit()
+{
+	printf("Test 3 Best Fit \n");
+	best_fit_memory_init(1000);
+
+	void *a = best_fit_alloc(200);
+	void *b = best_fit_alloc(220);
+	void *c = best_fit_alloc(220);
+	best_fit_alloc(200);
+
+	printf("Full\n");
+	printCurrentAllocs(1);
+	printf("---\n");
+
+	best_fit_dealloc(a);
+	printf("Freed block 1\n");
+	printCurrentAllocs(1);
+	printf("---\n");
+
+	best_fit_dealloc(c);
+	printf("Freed block 3\n");
+	printCurrentAllocs(1);
+	printf("---\n");
+
+	best_fit_dealloc(b);
+	printf("Freed block 2\n");
+	printCurrentAllocs(1);
+	printf("---\n");
+
+	return;
+}
+
+void test4_best_fit()
+{
+	printf("Test 4 Best Fit \n");
+	best_fit_memory_init(1000);
+
+	best_fit_alloc(201);
+	best_fit_alloc(203);
+	best_fit_alloc(203);
+	best_fit_alloc(203);
+
+	printCurrentAllocs(1);
+	printf("---\n");
+}
+
+void test5_best_fit()
+{
+	printf("Test 5 Best Fit \n");
+	best_fit_memory_init(1000);
+
+	best_fit_alloc(200);
+	best_fit_alloc(500);
+	best_fit_alloc(100);
+	printCurrentAllocs(1);
+	printf("---\n");
+
+	printf("Asking for the last block which is too much mem \n");
+	best_fit_alloc(300);
+
+	printCurrentAllocs(1);
+	printf("---\n");
+}
+
+//allocating/deallocating memory and looking at how many fragments
+void test6_best_fit()
+{
+	printf("Test 6 Best Fit \n");
+	best_fit_memory_init(1000);
+
+	best_fit_alloc(100);
+	void *b = best_fit_alloc(150);
+	best_fit_alloc(200);
+	best_fit_alloc(100);
+	best_fit_alloc(100);
+	best_fit_alloc(40);
+
+	best_fit_dealloc(b);
+	best_fit_alloc(100);
+
+	printf("Number of free blocks with under 100 bytes: %d \n", best_fit_count_extfrag(100));
+	printCurrentAllocs(1);
+	printf("---\n");
+}
+
+void test7_best_fit()
+{ // tests for invalid starting size (less than a control node)
+	printf("Test 7 Best Fit \n");
+	int result = best_fit_memory_init(30);
+	printf("0 if memory init worked, -1 if it did not. Should be -1: %d \n", result);
+	printf("---\n");
+}
+
+void test8_best_fit()
+{ //allocates some memory and uses it
+	printf("Test 8 Best Fit \n");
+	int result = best_fit_memory_init(1000);
+	if (result == -1)
+	{
 		return;
+	}
+	void *a = best_fit_alloc(10);
+	*(int *)a = 25;
+	printf("Should say 25: %d \n", *(int *)a);
+	printf("---\n");
 
+	void *b = best_fit_alloc(10);
+	*(int *)b = 20;
+
+	*(int *)a = *(int *)a + *(int *)b;
+	printf("Should say 45: %d \n", *(int *)a);
+	printf("---\n");
+
+	best_fit_dealloc(b);
+	printf("Should say 45: %d \n", *(int *)a);
+	printf("---\n");
+}
+
+//Tests wherther or not we can handle being passed an incorrrect start address for deallocation
+void test9_best_fit()
+{
+	printf("Test 9 Best Fit \n");
+	best_fit_memory_init(1000);
+
+	best_fit_alloc(200);
+	void *b = best_fit_alloc(220);
+	best_fit_alloc(220);
+	best_fit_alloc(200);
+
+	printf("Allocations: \n");
+	printCurrentAllocs(1);
+	printf("---\n");
+
+	printf("Trying to deallocate a block that is not the starting address we gave\n");
+	best_fit_dealloc(b + 12);
+	printCurrentAllocs(1);
+	printf("---\n");
+
+	printf("Deallocating the same block of memory by giving the correct start address\n");
+	best_fit_dealloc(b);
+	printCurrentAllocs(1);
+	printf("---\n");
+}
+
+void worst_vs_best_fragments()
+{ // allocates a lot of small blocks deallocates some and allocates small spaces, makes fragments in best fit
+	printf("Test Worst Fit Vs. Best Fit Fragments: Best Fit Case \n");
+	int result = best_fit_memory_init(1236);
+	if (result == -1)
+	{
+		return;
+	}
+
+	void *a = best_fit_alloc(52);
+	best_fit_alloc(4);
+	void *b = best_fit_alloc(100);
+	best_fit_alloc(4);
+
+	void *c = best_fit_alloc(52);
+	best_fit_alloc(4);
+	void *d = best_fit_alloc(100);
+	best_fit_alloc(4);
+
+	void *e = best_fit_alloc(52);
+	best_fit_alloc(4);
+	void *f = best_fit_alloc(100);
+	best_fit_alloc(4);
+
+	void *g = best_fit_alloc(52);
+	best_fit_alloc(4);
+	void *h = best_fit_alloc(100);
+
+	printf("After allocating \n");
+	printCurrentAllocs(1);
+	printf("---\n");
+
+	best_fit_dealloc(a);
+	best_fit_dealloc(b);
+	best_fit_dealloc(c);
+	best_fit_dealloc(d);
+	best_fit_dealloc(e);
+	best_fit_dealloc(f);
+	best_fit_dealloc(g);
+	best_fit_dealloc(h);
+
+	printf("After deallocating \n");
+	printCurrentAllocs(1);
+	printf("---\n");
+
+	best_fit_alloc(8);
+	best_fit_alloc(8);
+	best_fit_alloc(8);
+	best_fit_alloc(8);
+
+	printf("After allocating 8\n");
+	printCurrentAllocs(1);
+	printf("---\n");
+
+	printf("Best fit fragmentation count below 8 bytes: %d \n", best_fit_count_extfrag(8));
+	printf("---\n");
+
+	printf("Test Worst Fit Vs. Best Fit Fragments: Worst Fit Case \n");
+	int result2 = worst_fit_memory_init(1236);
+	if (result2 == -1)
+	{
+		return;
+	}
+
+	void *a2 = worst_fit_alloc(52);
+	worst_fit_alloc(4);
+	void *b2 = worst_fit_alloc(100);
+	worst_fit_alloc(4);
+
+	void *c2 = worst_fit_alloc(52);
+	worst_fit_alloc(4);
+	void *d2 = worst_fit_alloc(100);
+	worst_fit_alloc(4);
+
+	void *e2 = worst_fit_alloc(52);
+	worst_fit_alloc(4);
+	void *f2 = worst_fit_alloc(100);
+	worst_fit_alloc(4);
+
+	void *g2 = worst_fit_alloc(52);
+	worst_fit_alloc(4);
+	void *h2 = worst_fit_alloc(100);
+
+	printf("After allocating \n");
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	worst_fit_dealloc(a2);
+	worst_fit_dealloc(b2);
+	worst_fit_dealloc(c2);
+	worst_fit_dealloc(d2);
+	worst_fit_dealloc(e2);
+	worst_fit_dealloc(f2);
+	worst_fit_dealloc(g2);
+	worst_fit_dealloc(h2);
+
+	printf("After deallocating \n");
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	worst_fit_alloc(8);
+	worst_fit_alloc(8);
+	worst_fit_alloc(8);
+	worst_fit_alloc(8);
+
+	printf("After allocating 8 \n");
+	printCurrentAllocs(0);
+	printf("---\n");
+
+	printf("worst fit fragmentation count below 8 bytes: %d \n", worst_fit_count_extfrag(8));
+	printf("---\n");
+}
+
+void best_vs_worst_large_data()
+{
+	//create worst fit memory space with divider in middle
+	worst_fit_memory_init(2124);
+	void *a = worst_fit_alloc(1000);
+	worst_fit_alloc(4);
+	void *b = worst_fit_alloc(1000);
+	printCurrentAllocs(0);
+	worst_fit_dealloc(a);
+	worst_fit_dealloc(b);
+	printCurrentAllocs(0);
+	//create best fit memory space with divider in middle
+	best_fit_memory_init(2124);
+	void *c = best_fit_alloc(1000);
+	best_fit_alloc(4);
+	void *d = best_fit_alloc(1000);
+	printCurrentAllocs(0);
+	best_fit_dealloc(c);
+	best_fit_dealloc(d);
+	printCurrentAllocs(0);
+
+	int i;
+	for (i = 0; i < 16; i++)
+	{
+		worst_fit_alloc(20);
+		best_fit_alloc(20);
+	}
+
+	void *kB_worst = worst_fit_alloc(1000);
+	void *kB_best = best_fit_alloc(1000);
+
+	printf("worst kB: %lu best kB: %lu\n", (long unsigned int)kB_worst, (long unsigned int)kB_best);
 }
 
 int main(int argc, char *argv[])
 {
+	// test1_worst_fit();
+	// test2_worst_fit();
+	// test3_worst_fit();
+	// test4_worst_fit();
+	// test5_worst_fit();
+	// test6_worst_fit();
+	// test7_worst_fit();
+	// test8_worst_fit();
+	// test9_worst_fit();
 
-	test1_best_fit();
+	// test1_best_fit();
+	// test2_best_fit();
+	// test3_best_fit();
+	// test4_best_fit();
+	// test5_best_fit();
+	// test6_best_fit();
+	// test7_best_fit();
+	// test8_best_fit();
+	// test9_best_fit();
+
+	// worst_vs_best_fragments();
+	best_vs_worst_large_data();
 
 	return 0;
 }
-
-// int main(int argc, char *argv[])
-// {
-// 	int num = 0;
-// 	int algo = 0; // default algorithm to test is best fit  
-// 	void *p, *q;
-
-// 	if (argc != 2) {
-// 		fprintf(stderr, "Usage: %s <0/1>. 0 for best fit and 1 for worst fit \n", argv[0]);
-// 		exit (1);
-// 	} else if (!strcmp(argv[1], "1") || !strcmp(argv[1], "0")) {
-// 		algo = atoi(argv[1]);
-// 	} else {
-// 		fprintf(stderr, "Invalid argument, please specify 0 or 1\n");
-// 		exit(1);
-// 	}
-	
-// 	if ( algo == 0 ) {
-// 		best_fit_memory_init(1024);	// initizae 1KB, best fit
-
-// 		p = best_fit_alloc(8);		// allocate 8B
-// 		printf("best fit: p=%p\n", p);
-// 		if ( p != NULL ) {
-// 			best_fit_dealloc(p);	
-// 		}
-// 		num = best_fit_count_extfrag(4);
-// 	} else if ( algo == 1 ) {
-
-// 		worst_fit_memory_init(1024);	// initizae 1KB, worst fit
-
-// 		q = worst_fit_alloc(8);		// allocate 8B
-// 		printf("worst fit: q=%p\n", q);
-// 		if ( q != NULL ) {
-// 			worst_fit_dealloc(q);	
-// 		}
-// 		num = worst_fit_count_extfrag(4);
-// 	} else {
-// 		fprintf(stderr, "Should not reach here!\n");
-// 		exit(1);
-// 	}
-
-// 	printf("num = %d\n", num);
-
-// 	return 0;
-// }
--- lab4/starter/mem.c
+++ lab4/starter/mem.c
@@ -14,7 +14,7 @@
 struct ll_node {
 	int size; // size of memory
 	int state; // if the node is free or not 0 means free 1 means occupied
-	void *startingAddy;
+	void* startingAddy;
 	struct ll_node* previous; // points to previous node
 	struct ll_node* next; // points to next node
 
@@ -25,17 +25,17 @@ struct ll_node {
 void* listBest;
 void* listWorst;
 
-int totalMemoryBest;
-int totalMemoryWorst;
 /* Functions */
 
 /* memory initializer */
 int best_fit_memory_init(size_t size)
 {
-	totalMemoryBest = (int)size;
+	//Check if size is too small
+	if((int)size < sizeof(struct ll_node))
+		return -1;
 	listBest = malloc(size); // allocate the memory space
 
-	if(listBest == -1)
+	if((int)listBest == -1)
 		return -1;
 	
 	struct ll_node* first = listBest;
@@ -50,16 +50,20 @@ int best_fit_memory_init(size_t size)
 
 int worst_fit_memory_init(size_t size)
 {
-	totalMemoryWorst = (int)size;
+	//Check if size is too small
+	if((int)size < sizeof(struct ll_node))
+		return -1;
 	listWorst = malloc(sizeof((int)size));
-
+	if ((int)listWorst == -1)
+		return -1;
+	
 	struct ll_node *first = listWorst;
 	first -> size = (int)size - sizeof(struct ll_node); // whatever the remaining size of the memory space is will be the remaining space
 	first -> previous = NULL;
 	first -> next = NULL;
 	first -> state = 0;
-	first -> startingAddy = sizeOfMemAlloc(listWorst) + sizeof(struct ll_node);
-	
+	first->startingAddy = listWorst + sizeof(struct ll_node);
+
 	return 0;
 }
 
@@ -71,52 +75,106 @@ void *best_fit_alloc(size_t size_requested)
 	//Handle inavlid cases
 	if(size <= 0)
 		return NULL;
-	else if(size < sizeof(struct ll_node))
-		return NULL;
 
 	struct ll_node* best = NULL;
 	struct ll_node* move = listBest;
-	int diff = 0;
+	int diff;
 
 	while (move != NULL)
 	{
 		//Check if current block is free and big enough for requested allocation
-		if ((move -> size) >= size  && move->state == 0)
+		if ((move -> size) >= size  && move -> state == 0)
 		{
 			//If block hasnt been allocated or is smaller than last smallest block, then allocate space 
-			if(best == NULL || (move->size < best->size))
+			if(best == NULL || (move -> size < best -> size)){
 				best = move;
+			}	
 		}
 		move = move->next;
-	} // Find smallest memory block
+	} //Find smallest memory block
 
+	//Check if noadequate memory block was found
+	if( best == NULL){
+		return NULL;
+	}
 	best -> state = 1;
 	diff = (best->size) - size - sizeof(struct ll_node);
 
 	//Check if there is free space leftover
-	if (diff <= 0)
+	if (diff < 0)
 	{
+		best -> size = size;
 		return best->startingAddy;
 	}
-	else //Create node to control free space
+	else if(diff >= 0)//Create node to control free space
 	{
 		//Enough free space is avaialbe to create free space node
 		struct ll_node* freeSpace = best->startingAddy + size;
-		freeSpace->size = diff;
-		freeSpace->state = 0;
-		freeSpace->startingAddy = freeSpace + 1;
-		freeSpace->next == NULL;
-		freeSpace->previous == NULL;
+		freeSpace -> size = diff;
+		freeSpace -> state = 0;
+		freeSpace -> startingAddy = freeSpace + 1;
+		freeSpace -> next == NULL;
+		freeSpace -> previous == NULL;
 
-		insertNode(best, freeSpace);
-		return best -> startingAddy;
+		best -> size = size;
+		insert_node(best, freeSpace);
 	}
+	return best -> startingAddy;
 }
 
-void *worst_fit_alloc(size_t size)
+void *worst_fit_alloc(size_t size_requested)
 {
 	// To be completed by students
-	return NULL;
+	int size = makeDivisibleByFour((int)size_requested);
+	//Handle inavlid cases
+	if (size <= 0)
+		return NULL;
+	
+	struct ll_node *worst = NULL; 
+	struct ll_node *move = listWorst;
+	int diff;
+
+	while (move != NULL)
+	{
+		//Check if current block is free and big enough for requested allocation
+		if ((move -> size) >= size && move -> state == 0)
+		{
+			//If block hasnt been allocated or is smaller than last smallest block, then allocate space
+			if (worst == NULL || (move -> size > worst -> size))
+			{
+				worst = move;
+			}
+		}
+		move = move->next;
+	} // Find smallest memory block
+
+	//Check if no adequate memory block was found
+	if( worst == NULL){
+		return NULL;
+	}
+	worst->state = 1;
+	diff = (worst->size) - size - sizeof(struct ll_node);
+
+	//Check if there is free space leftover
+	if (diff < 0)
+	{
+		worst -> size = size;
+		return worst -> startingAddy;
+	}
+	else if (diff >= 0) //Create node to control free space
+	{
+		//Enough free space is avaialbe to create free space node
+		struct ll_node *freeSpace = worst -> startingAddy + size;
+		freeSpace -> size = diff;
+		freeSpace -> state = 0;
+		freeSpace -> startingAddy = freeSpace + 1;
+		freeSpace -> next == NULL;
+		freeSpace -> previous == NULL;
+
+		worst->size = size;
+		insert_node(worst, freeSpace);
+	}
+	return worst->startingAddy;
 }
 
 /* memory de-allocator */
@@ -128,7 +186,6 @@ void best_fit_dealloc(void *ptr)
 		return;
 	}
 		
-	
 	struct ll_node* traverse = listBest;
 	//Find node to de-alloc
 	while(traverse != NULL){
@@ -141,31 +198,61 @@ void best_fit_dealloc(void *ptr)
 		printf("Did not find ptr in list\n");
 		return;
 	}
-		
-
+	
 	//Check if there is free space to me merged together
 	//Check if there is a free node 'next'
-	if(traverse -> next -> state == 0 && traverse -> next != NULL){
+	if( traverse -> next != NULL && traverse -> next -> state == 0){
 		traverse -> state = 0;
 		traverse -> size += traverse -> next -> size + sizeof(struct ll_node);
-		removeNode(traverse -> next);						
+		removeNode(traverse -> next);					
 	}
 	//Check if there is a free node 'previous'
-	else if(traverse -> previous -> state == 0 && traverse -> previous != NULL){
+	else if(traverse -> previous != NULL && traverse -> previous -> state == 0){
 		traverse -> previous -> size += traverse -> size + sizeof(struct ll_node);
 		removeNode(traverse);
 	}	
 	else
-		printf("%i", traverse -> state);
 		traverse -> state = 0;
-		printf("%i", traverse->state);
-		return;
+	return;
 }
 
 void worst_fit_dealloc(void *ptr) 
 {
-
 	// To be completed by students
+	if(ptr == NULL){
+		printf("Pointer passed in was NULL \n");
+		return;
+	}
+		
+	struct ll_node* traverse = listWorst;
+	//Find node to de-alloc
+	while(traverse != NULL){
+		if(traverse -> startingAddy == ptr){
+			break;												
+		}
+		traverse = traverse -> next;
+	}
+	if(traverse == NULL){
+		printf("Did not find ptr in list\n");
+		return;
+	}
+	
+	//Check if there is free space to be merged together
+	//Check if there is a free node 'next'
+	if( traverse -> next != NULL && traverse -> next -> state == 0){
+		traverse -> state = 0;
+		traverse -> size += traverse -> next -> size + sizeof(struct ll_node);
+		removeNode(traverse -> next);		
+	}
+	//Check if there is a free node 'previous'
+	else if(traverse -> previous != NULL && traverse -> previous -> state == 0){
+		traverse -> previous -> size += traverse -> size + sizeof(struct ll_node);
+		removeNode(traverse);
+	}	
+	else{ //No free nodes nearby to merge
+		traverse -> state = 0;
+	}
+		
 	return;
 }
 
@@ -175,13 +262,27 @@ void worst_fit_dealloc(void *ptr)
 int best_fit_count_extfrag(size_t size)
 {
 	// To be completed by students
-	return 0;
+	int count = 0;
+	struct ll_node* traverse = listBest;
+	while(traverse != NULL){
+		if(traverse -> size < (int)size && traverse -> state == 0)
+			count++;
+		traverse= traverse -> next;
+	}
+	return count;
 }
 
 int worst_fit_count_extfrag(size_t size)
 {
 	// To be completed by students
-	return 0;
+	int count = 0;
+	struct ll_node* traverse = listWorst;
+	while(traverse != NULL){
+		if(traverse -> size < (int)size && traverse -> state == 0)
+			count++;
+		traverse= traverse -> next;
+	}
+	return count;
 }
 
 //Return size of memory currently allocated in stack
@@ -219,7 +320,7 @@ void printCurrentAllocs(int bestOrWorst){
 }
 
 // inserts node to linked list
-void insertNode(struct ll_node* previousnode , struct ll_node *insertnode){
+void insert_node(struct ll_node* previousnode , struct ll_node* insertnode){
 	insertnode -> next = previousnode -> next;
 	previousnode -> next = insertnode;
 	insertnode -> previous  = previousnode;
