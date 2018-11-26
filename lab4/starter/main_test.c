44c72f34078ec2e89417833245f747723a00a1ad
Daivik Goel <daivikgoel98@gmail.com>
2018-11-26 15:53:40 -0500

    Added more test cases

--- lab4/starter/main_test.c
+++ lab4/starter/main_test.c
@@ -20,7 +20,7 @@
 // tests completely filling the mem space and deallocates two blocks and then allocates one into the larger free block
 void worst_fit1()
 {
-    printf("Worst Fit Test 1\n");
+    printf("Worst Fit Test 1 Testing Alloc and Dealloc\n");
     worst_fit_memory_init(600);
 
     void *a = worst_fit_alloc(105);
@@ -49,22 +49,22 @@ void worst_fit1()
 
 void worst_fit2()
 {
-    printf("Worst Fit Test 2 \n");
+    printf("Worst Fit Test 2 Testing Coalescing\n");
     worst_fit_memory_init(1000);
 
     void *a = worst_fit_alloc(100);
     void *b = worst_fit_alloc(100);
     void *c = worst_fit_alloc(100);
-    //void *d = worst_fit_alloc(100);
+    void *d = worst_fit_alloc(100);
 
     printf("Full\n");
     printCurrentAllocs(0);
     printf("---\n");
 
     worst_fit_dealloc(a);
-    //worst_fit_dealloc(b);
-    worst_fit_dealloc(c);
     worst_fit_dealloc(b);
+    worst_fit_dealloc(c);
+    worst_fit_dealloc(d);
 
     printf("Deallocating all blocks to see if deallocations is working \n");
     printCurrentAllocs(0);
@@ -74,7 +74,7 @@ void worst_fit2()
 }
 void worst_fit3()
 {
-    printf("Worst Fit Test 3 \n");
+    printf("Worst Fit Test 3 Testing difference between worst and best\n");
     worst_fit_memory_init(1000);
 
     void *a = worst_fit_alloc(100);
@@ -104,10 +104,40 @@ void worst_fit3()
 
     return;
 }
+void worst_fit4()
+{
+    printf("Worst Fit Test 4 Testing Fragmentation code\n");
+    worst_fit_memory_init(1000);
+
+    void *a = worst_fit_alloc(100);
+    void *b = worst_fit_alloc(100);
+    void *c = worst_fit_alloc(110);
+    void *d = worst_fit_alloc(100);
+
+    printf("Full\n");
+    printCurrentAllocs(0);
+    printf("---\n");
+
+    void *h = worst_fit_alloc(4);
+    void *i = worst_fit_alloc(4);
+    void *j = worst_fit_alloc(4);
+    void *k = worst_fit_alloc(4);
+    void *l = worst_fit_alloc(4);
+    worst_fit_dealloc(h);
+    worst_fit_dealloc(k);
+    printf("Should only have 2 data fragments below 8\n");
+    printCurrentAllocs(0);
+    printf("---\n");
+
+    printf("Worst fit fragments under 8 bytes: %d \n", worst_fit_count_extfrag(8));
+    printf("---\n");
+
+    return;
+}
 
 void best_fit1()
 {
-    printf("Best Fit Test 1 \n");
+    printf("Best Fit Test 1 Testing Alloc and Dealloc\n");
     best_fit_memory_init(600);
 
     void *a = best_fit_alloc(105);
@@ -134,9 +164,9 @@ void best_fit1()
     return;
 }
 
-void best_fit2()
+void best_fit2() // Testing Coalescing
 {
-    printf("Best Fit Test 2 \n");
+    printf("Best Fit Test 2 Testing Coalescing \n");
     best_fit_memory_init(1000);
 
     void *a = best_fit_alloc(100);
@@ -162,7 +192,7 @@ void best_fit2()
 
 void best_fit3()
 {
-    printf("Best Fit Test 3 \n");
+    printf("Best Fit Test 3 Testing difference between worst and best \n");
     best_fit_memory_init(1000);
 
     void *a = best_fit_alloc(100);
@@ -188,118 +218,38 @@ void best_fit3()
 
     return;
 }
-void worst_vs_best_fragments()
-{ // allocates a lot of small blocks deallocates some and allocates small spaces, makes fragments in best fit
-    printf("Test Worst Fit Vs. Best Fit Fragments: Best Fit Case \n");
-    int result = best_fit_memory_init(1236);
-    if (result == -1)
-    {
-        return;
-    }
 
-    void *a = best_fit_alloc(52);
-    best_fit_alloc(4);
+void best_fit4()
+{
+    printf("Worst Fit Test 4 Testing Fragmentation code\n");
+    best_fit_memory_init(1000);
+
+    void *a = best_fit_alloc(100);
     void *b = best_fit_alloc(100);
-    best_fit_alloc(4);
-
-    void *c = best_fit_alloc(52);
-    best_fit_alloc(4);
+    void *c = best_fit_alloc(110);
     void *d = best_fit_alloc(100);
-    best_fit_alloc(4);
 
-    void *e = best_fit_alloc(52);
-    best_fit_alloc(4);
-    void *f = best_fit_alloc(100);
-    best_fit_alloc(4);
-
-    void *g = best_fit_alloc(52);
-    best_fit_alloc(4);
-    void *h = best_fit_alloc(100);
-
-    printf("After allocating \n");
+    printf("Full\n");
     printCurrentAllocs(1);
     printf("---\n");
 
-    best_fit_dealloc(a);
-    best_fit_dealloc(b);
-    best_fit_dealloc(c);
-    best_fit_dealloc(d);
-    best_fit_dealloc(e);
-    best_fit_dealloc(f);
-    best_fit_dealloc(g);
+    void *h = best_fit_alloc(4);
+    void *i = best_fit_alloc(4);
+    void *j = best_fit_alloc(4);
+    void *k = best_fit_alloc(4);
+    void *l = best_fit_alloc(4);
     best_fit_dealloc(h);
-
-    printf("After deallocating \n");
+    best_fit_dealloc(k);
+    printf("Should only have 2 data fragments below 8\n");
     printCurrentAllocs(1);
     printf("---\n");
 
-    best_fit_alloc(8);
-    best_fit_alloc(8);
-    best_fit_alloc(8);
-    best_fit_alloc(8);
-
-    printf("After allocating 8\n");
-    printCurrentAllocs(1);
+    printf("Worst fit fragments under 8 bytes: %d \n", best_fit_count_extfrag(8));
     printf("---\n");
 
-    printf("Best fit fragmentation count below 8 bytes: %d \n", best_fit_count_extfrag(8));
-    printf("---\n");
-
-    printf("Test Worst Fit Vs. Best Fit Fragments: Worst Fit Case \n");
-    int result2 = worst_fit_memory_init(1236);
-    if (result2 == -1)
-    {
-        return;
-    }
-
-    void *a2 = worst_fit_alloc(52);
-    worst_fit_alloc(4);
-    void *b2 = worst_fit_alloc(100);
-    worst_fit_alloc(4);
-
-    void *c2 = worst_fit_alloc(52);
-    worst_fit_alloc(4);
-    void *d2 = worst_fit_alloc(100);
-    worst_fit_alloc(4);
-
-    void *e2 = worst_fit_alloc(52);
-    worst_fit_alloc(4);
-    void *f2 = worst_fit_alloc(100);
-    worst_fit_alloc(4);
-
-    void *g2 = worst_fit_alloc(52);
-    worst_fit_alloc(4);
-    void *h2 = worst_fit_alloc(100);
-
-    printf("After allocating \n");
-    printCurrentAllocs(0);
-    printf("---\n");
-
-    worst_fit_dealloc(a2);
-    worst_fit_dealloc(b2);
-    worst_fit_dealloc(c2);
-    worst_fit_dealloc(d2);
-    worst_fit_dealloc(e2);
-    worst_fit_dealloc(f2);
-    worst_fit_dealloc(g2);
-    worst_fit_dealloc(h2);
-
-    printf("After deallocating \n");
-    printCurrentAllocs(0);
-    printf("---\n");
-
-    worst_fit_alloc(8);
-    worst_fit_alloc(8);
-    worst_fit_alloc(8);
-    worst_fit_alloc(8);
-
-    printf("After allocating 8 \n");
-    printCurrentAllocs(0);
-    printf("---\n");
-
-    printf("worst fit fragmentation count below 8 bytes: %d \n", worst_fit_count_extfrag(8));
-    printf("---\n");
+    return;
 }
+
 
 void best_vs_worst_large_data()
 {
@@ -354,13 +304,15 @@ void best_vs_worst_large_data()
 
 int main(int argc, char *argv[])
 {
-    // worst_fit1();
-    //worst_fit2();
+     worst_fit1();
+    worst_fit2();
     worst_fit3();
-    // best_fit1();
-    //best_fit2();
+    worst_fit4();
+    best_fit1();
+    best_fit2();
     best_fit3();
-    // worst_vs_best_fragments();
+    best_fit4();
+ 
     //best_vs_worst_large_data();
 
     return 0;
